6df6425d16e34d5422ccc5e08db3f26b94478584
SnappyJensen <mikk4388@edu.campusvejle.dk>
2017-12-06 11:31:39 +0100

    Added summaries. Added the Append method. Added a unit test (SummaryTest) not fully realised yet.

---
+++ AspIT.BoardManagement.Tests.EntitiesTests/SummaryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspIT.BoardManagement.Tests.EntitiesTests
+{
+	[TestClass]
+	public class SummaryTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			//Arrange:
+
+			//Act:
+
+			//Assert:
+		}
+	}
+}
