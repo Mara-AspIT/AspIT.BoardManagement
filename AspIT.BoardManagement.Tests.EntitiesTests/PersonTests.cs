d9e138f98123adb8e69ea8782753f826931e34de
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-12-07 14:01:33 +0100

    Added birthdate setter tests

--- AspIT.BoardManagement.Tests.EntitiesTests/PersonTests.cs
+++ AspIT.BoardManagement.Tests.EntitiesTests/PersonTests.cs
@@ -639,6 +639,61 @@ namespace AspIT.BoardManagement.Tests.EntitiesTests
         #endregion
 
         #region Birthday setter tests
+        /// <summary>
+        /// Tests the birthdate setter.
+        /// </summary>
+        [TestMethod]
+        public void TestBirthdaySetterCorrect()
+        {
+            // Arrange
+            DateTime expected = new DateTime(1992, 1, 9);
+
+            const string firstName = "Jeff";
+            const string lastName = "Camel";
+            DateTime birthdate = DateTime.Now;
+            const string address = "North street 34";
+            const string city = "Blankton city";
+            const string region = "North region";
+            const string postalCode = "3991";
+            const string country = "Saint Country";
+            ContactInfo contactInfo = new ContactInfo("Krrep@rle.dl", "2929319");
+            Person person;
+
+            // Act
+            person = new Person(firstName, lastName, birthdate, address, city, region, postalCode, country, contactInfo)
+            {
+                BirthDate = expected
+            };
+
+            // Assert
+            Assert.AreEqual(expected, person.BirthDate);
+        }
+
+        /// <summary>
+        /// Tests the birthdate setter. Passes if <see cref="ArgumentException"/> is thrown.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBirthdaySetterIncorrect1()
+        {
+            // Arrange
+            const string firstName = "Jeff";
+            const string lastName = "Camel";
+            DateTime birthdate = DateTime.Now;
+            const string address = "North street 34";
+            const string city = "Blankton city";
+            const string region = "North region";
+            const string postalCode = "3991";
+            const string country = "Saint Country";
+            ContactInfo contactInfo = new ContactInfo("Krrep@rle.dl", "2929319");
+            Person person;
+
+            // Act
+            person = new Person(firstName, lastName, birthdate, address, city, region, postalCode, country, contactInfo)
+            {
+                BirthDate = new DateTime(2100, 2, 3)
+            };
+        }
         #endregion
 
         #region Address setter tests
