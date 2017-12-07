911dd50c6cf4718355b5a4937b2574149fc0218d
emil georgi <emil.georgi90@gmail.com>
2017-12-07 13:40:36 +0100

    added header to boardMeeting

--- AspIT.BoardManagement.Entities/BoardMeeting.cs
+++ AspIT.BoardManagement.Entities/BoardMeeting.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿/*********************************************************************************************
+*  Author: Emil Georgi (emil376g@edu.campusvejle.dk), github: https://github.com/emil376g/   *
+*  Solution: .NET version: 4.7.1, C# version: 7.1                                            *
+*  Visual Studio version: Visual Studio Enterprise 2017, version 15.4.5                      *
+*  Repository: https://github.com/Mara-AspIT/AspIT.Bms.git                                   *
+*********************************************************************************************/
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AspIT.BoardManagement.Entities
 {
