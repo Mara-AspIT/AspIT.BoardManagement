9de05e3dd4a5f44973709f562a6e3dd843104013
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-12-12 11:34:41 +0100

    Made a create agenda method. Made some changes to the Equals method

--- AspIT.BoardManagement.Entities/Chairman.cs
+++ AspIT.BoardManagement.Entities/Chairman.cs
@@ -54,6 +54,15 @@ namespace AspIT.BoardManagement.Entities
         }
 
         /// <summary>
+        /// Creates a agenda.
+        /// </summary>
+        /// <param name="agenda">The agenda to create.</param>
+        public void CreateAgenda(Agenda agenda)
+        {
+            // TODO: Create agenda logic
+        }
+
+        /// <summary>
         /// Determines whether this <see cref="Chairman"/> instance is the same as the other <see cref="Chairman"/> instance, and have the same unique ID.
         /// </summary>
         /// <param name="other">The other <see cref="Chairman"/> to compare with.</param>
@@ -61,17 +70,7 @@ namespace AspIT.BoardManagement.Entities
         public bool Equals(Chairman other)
         {
             if(ReferenceEquals(this, other)) return true;
-            return Id == other.Id
-                && other.FirstName == FirstName
-                && other.LastName == LastName
-                && other.BirthDate == BirthDate
-                && other.Address == Address
-                && other.City == City
-                && other.Region == Region
-                && other.PostalCode == PostalCode
-                && other.Country == Country
-                && other.ContactInfo == ContactInfo
-                && other.UserCredentials == UserCredentials;
+            return Id == other.Id;
         }
 
         /// <summary>
